9fa37c9b61fea47931e5398af73997660b1b5a43
Khaza Shaik <38318699+khazaShaik@users.noreply.github.com>
2019-01-24 13:20:22 +0530

    Create SignUpForm.java

---
+++ Symbo/Symbo_Bank_Project/Symbo_Bank/src/main/java/com/symbo/message/request/SignUpForm.java
@@ -0,0 +1,86 @@
+package com.symbo.message.request;
+
+import java.util.Set;
+
+import javax.validation.constraints.*;
+
+public class SignUpForm {
+   
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NotBlank
+    @Size(max = 60)
+    @Email
+    private String email;
+    
+    private String role;
+    
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+
+    private String firstName;
+    private String lastName;
+    private String phone;
+
+    
+    public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getRole() {
+    	return this.role;
+    }
+    
+    public void setRole(String role) {
+    	this.role = role;
+    }
+}
